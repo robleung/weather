cb564aa2636e4e0e58ef120ad25111195c4a3c88
Robert Leung <rleung84@uwo.ca>
2023-09-25 19:50:18 -0400

    added weather api fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import "./style.css";
 
-console.log("test");
+async function fetchForcast() {
+  let location = document.querySelector("#locationInput").value;
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=${process.env.apiKey}&q=${location}&days=1&aqi=no&alerts=no
+  `,
+    { mode: "cors" }
+  );
+  const forcastData = await response.json();
+  console.log(forcastData.forecast.forecastday[0].astro);
+}
+
+// function handleForcast(data) {
+//   console.log(data);
+// }
+
+// function updateForcast() {
+//   handleForcast(fetchForcast().);
+// }
 
 function app() {
   let body = document.querySelector("body");
@@ -8,9 +25,13 @@ function app() {
   title.innerHTML = "Will there be a good sunset?";
   let locationInput = document.createElement("input");
   locationInput.placeholder = "Search for a location";
-  body.append(title, locationInput);
+  locationInput.id = "locationInput";
+  let locationSubmit = document.createElement("button");
+  locationSubmit.innerHTML = "Submit";
+  locationSubmit.addEventListener("click", fetchForcast);
+  body.append(title, locationInput, locationSubmit);
 }
 //Build a website to predict sunset viewing probablility
 
 app();
-console.log(process.env.apiKey);
+// console.log(process.env.apiKey);
